3f36431edcd7f40afad67f2d4f8a66f77f4e8db5
mariehardy <mademoiselle.mph@gmail.com>
2020-05-30 17:16:16 +0200

    BERWDPT Marie - iteration 2 almost

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+import Contacts from './Components/Contacts'
 
-function App() {
+// let contactArray = [...contacts.slice(0,4)]
+// console.log("contactArray" + contactArray)
+
+class App extends Component {
+
+
+
+
+
+//   for adding a random contact you probably want to 
+//   (1) create a copy of the current array 
+//   (2) push to that copy 
+//   (3) set that copy as the new contacts array in the state
+// (or you find a method that does not mutate the original array but 
+// returns a new array with one element added )
+
+//concat    <===== use this instead of MATH.RANDOM
+
+// _.sample(contacts) is great to get one random one
+// but then you still need to add it to the existing ones 
+// and then set that as the new state
+
+  addRandomHandler = () => {
+
+    this.setState({
+      contacts: contacts.concat('randomCelebirty!!!') 
+    })
+  }
+
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>ironContacts</h1>
+    <button onClick={this.addRandomHandler}>Add Random Contact</button>
+    <Contacts key={contacts.id}></Contacts>
     </div>
   );
+  
+}
 }
 
 export default App;
---
+++ src/Components/Contacts.js
@@ -0,0 +1,44 @@
+//rfce + tab ==> function component
+//rcc + tab ==> class Component
+
+import React, { Component } from 'react';
+import contacts from '../contacts.json';
+
+export default class Contacts extends Component {
+  state = {
+    contacts: contacts.slice(0, 5),
+  };
+  render() {
+    return (
+      <table className="contactTable">
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        {this.state.contacts.map((c) => {
+          return [
+
+            <tr>
+              <td>
+                <img
+                  className="contactTable__img"
+                  src={c.pictureUrl}
+                  alt={c.name}
+                />
+              </td>
+              <td>
+                <p>{c.name}</p>
+              </td>
+              <td>
+                <p>{Math.round(c.popularity * 100) / 100}</p>
+              </td>
+            </tr>,
+          ];
+        })}
+      </table>
+    );
+  }
+}
