0bbbeebbc6d761f37b44f66c70fb04b1de649471
mariehardy <mademoiselle.mph@gmail.com>
2020-06-02 21:24:03 +0200

    berwdpt marie - variable names changed

--- src/App.js
+++ src/App.js
@@ -6,15 +6,6 @@ import Contacts from './Components/Contacts';
 import _ from 'lodash';
 
 class App extends Component {
-  // state = {
-  //   contacts: contactsFromJson.slice(0, 5),
-  // };
-
-
-
-
-
-  // };
 
   render() {
     
@@ -22,25 +13,6 @@ class App extends Component {
       <div className="App">
         <h1>ironContacts</h1>
        <Contacts />
-        {/* <table className="contactTable">
-          <thead>
-            <tr>
-              <th>Picture</th>
-              <th>Name</th>
-              <th>Popularity</th>
-            </tr>
-          </thead>
-          {this.state.contacts.map((c) => (
-            <Contacts
-              key={c.id}
-              contactId={c.id}
-              pictureUrl={c.pictureUrl}
-              popularity={c.popularity}
-              name={c.name}
-            />
-          ))}
-        </table> */}
-
       </div>
     );
   }
--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -2,7 +2,7 @@
 //rcc + tab ==> class Component
 
 import React, { Component } from 'react';
-import contactsFromJson from '../contacts.json';
+import allContacts from '../contacts.json';
 import Card from './Card';
 import _ from 'lodash';
 
@@ -10,55 +10,45 @@ export default class Contacts extends Component {
   constructor() {
     super();
     this.state = {
-      contacts: contactsFromJson.slice(0, 5),
+      contactsArr: allContacts.slice(0, 5),
     };
   }
 
-  //   To add a random contact you probably want to:
-  //   (1) create a copy of the current array
-  //   (2) push to that copy
-  //   (3) set that copy as the new contacts array in the state
-  // (or you find a method that does not mutate the original array but
-  // returns a new array with one element added )
-
-  //concat    <===== use this instead of PUSH
-
-  // _.sample(contacts) is great to get one random one
-  // but then you still need to add it to the existing ones
-  // and then set that as the new state
-
   addRandomHandler = () => {
-    let randomCelebrity = _.sample(contactsFromJson);
-    if (!_.find(this.state.contacts, { id: randomCelebrity.id })) {
+    // Either we create a copy of the array and then push to (mutate) the original array.
+    // let contactsArrCopy = [...this.state.contactsArr]
+    // contactsArrCopy.push(randomCelebrity)
+    let randomCelebrity = _.sample(allContacts);
+    if (!_.find(this.state.contactsArr, { id: randomCelebrity.id })) {
       this.setState({
-        contacts: this.state.contacts.concat(randomCelebrity),
+        contactsArr: this.state.contactsArr.concat(randomCelebrity),
       });
     }
   };
 
   sortByNameHandler = () => {
     this.setState({
-      contacts: _.sortBy(this.state.contacts, 'name'),
+      contactsArr: _.sortBy(this.state.contactsArr, 'name'),
     });
   };
 
   sortByPopularityHandler = () => {
     this.setState({
-      contacts: _.sortBy(this.state.contacts, 'popularity').reverse()
+      contactsArr: _.sortBy(this.state.contactsArr, 'popularity').reverse()
     });
   };
 
   deleteMovieHandler = (id) => {
-    const contactsCopy = [...this.state.contacts];
+    const contactsCopy = [...this.state.contactsArr];
     const contactsIndex = contactsCopy.findIndex((c) => c.id === id);
     contactsCopy.splice(contactsIndex, 1);
     this.setState({
-      contacts: contactsCopy,
+      contactsArr: contactsCopy,
     });
   };
 
   render() {
-    console.log(this.state.contacts);
+    console.log(this.state.contactsArr);
 
     return (
       <table className="contactTable">
@@ -75,7 +65,7 @@ export default class Contacts extends Component {
             <th>Popularity</th>
           </tr>
         </thead>
-        {this.state.contacts.map((c) => {
+        {this.state.contactsArr.map((c) => {
           return (
             <Card
               key={c.id}
